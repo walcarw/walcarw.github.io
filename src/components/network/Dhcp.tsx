0e6da0d5ca78f3267001040370e4041e4ee93ff0
WilliamWalca <132449932+walcarw@users.noreply.github.com>
2024-08-08 11:15:48 +0200

    Add notes about DHCP

--- src/components/network/Dhcp.tsx
+++ src/components/network/Dhcp.tsx
@@ -1,7 +1,115 @@
 function Dhcp(){
     return(
         <div>
-            <h2>DHCP Protocol</h2>
+            <h2>DHCP : Dynamic Host Configuration Protocol</h2>
+            <h3>Overview</h3>
+            <ul>
+                <li>GOAL : Automatically gives IP addresses to hosts</li>
+                <li>Uses the client-server architecture. The host send a request to a DHCP server and the server respond</li>
+                <li>
+                    Advantages
+                    <ul>
+                        <li>Centralize management of IP addresses</li>
+                        <li>Can reuse IP addresses</li>
+                        <li>Avoid manual configuration of IP address</li>
+                    </ul>
+                </li>
+                <li>
+                    Actors 
+                    <ul>
+                        <li>DHCP Client</li>
+                        <li>DHCP Server</li>
+                        <li>DHCP Relay</li>
+                    </ul>
+                </li>
+                <li>
+                    IP Addresses 
+                    <ul>
+                        <li>The DHCP server gives an IP address using a scope. A scope is a range of IP addresses and is determined by a start IP address and a end IP address.</li>
+                        <li>The DHCP server assigns IP addresses as a lease</li>
+                        <li>The DHCP server can reserve IP addresses for a special host so that the host have always the same IP address</li>
+                        <li>The MAC address is used to identify a host</li>
+                    </ul>
+                </li>
+                <li>
+                    Technical Infos 
+                    <ul>
+                        <li>Uses the protocol UDP</li>
+                        <li>Uses port 67 on the server</li>
+                        <li>Uses port 68 on the client(host)</li>
+                        <li>
+                            Informations given by the DHCP server 
+                            <ul>
+                                <li>The subnet mask</li>
+                                <li>IP of the default Gateway</li>
+                                <li>IP of the DNS server</li>
+                            </ul>
+                        </li>
+                        <li>
+                            Messages used
+                            <table>
+                                <tr>
+                                    <th>Message</th>
+                                    <th>Use case</th>
+                                </tr>
+
+                                <tr>
+                                    <td>DHCPDISCOVER</td>
+                                    <td>Sent when the client want to contact a DHCP server</td>
+                                </tr>
+
+                                <tr>
+                                    <td>DHCPOFFER</td>
+                                    <td>Sent when the server propose an IP address to the client</td>
+                                </tr>
+
+                                <tr>
+                                    <td>DHCPREQUEST</td>
+                                    <td>Sent when the client request the proposed IP address</td>
+                                </tr>
+
+                                <tr>
+                                    <td>DHCPACK</td>
+                                    <td>Sent when the server confirm the allocation of the IP address</td>
+                                </tr>
+
+                                <tr>
+                                    <td>DHCPNACK</td>
+                                    <td>Negative acknoledgement</td>
+                                </tr>
+
+                                <tr>
+                                    <td>DHCPDECLINE</td>
+                                    <td>Sent when the client decline an IP address offer</td>
+                                </tr>
+
+                                <tr>
+                                    <td>DHCPRELEASE</td>
+                                    <td>Sent when the client want to signal the server to release the IP address</td>
+                                </tr>
+
+                                <tr>
+                                    <td>DHCPINFORM</td>
+                                    <td>Sent when the client want to inform the server that its IP address was set manually</td>
+                                </tr>
+                            </table>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+
+            <h3>Scenarios</h3>
+            <ul>
+                <li>
+                    A Host want to receive an IP address 
+                    <ol>
+                        <li>The client send a DHCPDISCOVER. The message is broadcasted in the network.</li>
+                        <li>The server receive the DHCPDISCOVER message. The server reserve an IP address for the client. Then the server send a DHCPOFFER message to the client. The message contains the IP address that the server offer to the client.</li>
+                        <li>The client receive the DHCPOFFER message. The client send a DHCPREQUEST message to the server.</li>
+                        <li>The server receive the DHCPREQUEST message. The server assigns the address to the client. Then it send a DHCPACK message to the client.</li>
+                    </ol>
+                </li>
+            </ul>
         </div>
     );
 }
