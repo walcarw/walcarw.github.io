e21761cb64c80d62392b04cda23996d474ef15fc
WilliamWalca <132449932+walcarw@users.noreply.github.com>
2024-08-08 13:59:36 +0200

    Add notes about NAT

--- src/components/network/Nat.tsx
+++ src/components/network/Nat.tsx
@@ -1,7 +1,20 @@
 function Nat(){
     return(
         <div>
-            <h2>NAT Protocol</h2>
+            <h2>NAT : Network Address Translation</h2>
+            <h3>Overview</h3>
+            <ul>
+                <li>Goal : gives one IPv4 address to a local network. Devices in the local network have private IPv4 addresses and the router of the local network have one public IPv4 address.</li>
+                <li>
+                    Advantages
+                    <ul>
+                        <li>ISP need to give less IP addresses</li>
+                        <li>Can change addresses of devices in local network without notifying the outside world</li>
+                        <li>Can change ISP without changing addresses of devices in local network</li>
+                        <li>Devices in local network are not visible from outside (security)</li>    
+                    </ul>    
+                </li>
+            </ul>
         </div>
     );
 }
