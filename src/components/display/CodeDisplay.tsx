393c11b56b61acee8e66823ea6eda223b009227c
WilliamWalca <132449932+walcarw@users.noreply.github.com>
2024-08-20 17:21:09 +0200

    Update CodeDisplay

--- src/components/display/CodeDisplay.tsx
+++ src/components/display/CodeDisplay.tsx
@@ -3,12 +3,27 @@ interface Code{
     code: string
 }
 
+const keywords = ["const","var"]
+const datatypes = ["int", "float32"]
+
+function isPlaceHolder(word: string): boolean{
+    return word.charAt(0) === "<" && word.charAt(word.length-1) === ">";
+}
+
 function CodeDisplay(props: Code){
     return(
         <div className="code-displayer">
-            <pre>
-                {props.code}
-            </pre>
+            {
+                props.code.split("\n")
+                .map(line => <div>
+                    {line.split(" ").map(word => {
+                        if (keywords.includes(word)){return <span className="keyword">{word + " "}</span>}
+                        else if (datatypes.includes(word)){return <span className="datatype">{word + " "}</span>}
+                        else if (isPlaceHolder(word)){return <span className="placeholder">{word + " "}</span>}
+                        return <span>{word + " "}</span>
+                    })}
+                </div>)
+            }
         </div>
     );
 }
